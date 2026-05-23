e693fb9dae5a3ed140bfbfc7fe9a3007a7b3dc5d
Railton-AP-De-Paula-Web-designer <depaularailton91@gmail.com>
2026-05-23 02:29:12 -0300

    fruta js card teck

---
+++ js/nutri.js
@@ -0,0 +1,24 @@
+/* ==========================================================================
+   [[ CONTROLE DE DADOS NUTRICIONAIS DINÂMICOS - DISTRIBUIDORA VITÓRIA ]]
+   ========================================================================== */
+
+//1. banco de Dados Local das Frutas
+
+const dadosFrutas = {
+    "morango": {
+        titulo: "MORANGO",
+        imagem: "./assets/nutri-morango.png",
+        beneficios: [
+            { icone: "🌱", texto: "<strong>Vitamina C:</strong>Poderoso antioxidante que fortalece a imunidade e estimula o colágeno."},
+            { icone: "💧", texto: "<strong>Potássio:</strong> Essencial para o equilíbrio hídrico e controle da pressão arterial."},
+            { icone: "🍓", texto: "<strong>Energia Sustentada:</strong> Carboidratos de absorção lenta que evitam picos repentinos de glicose." },
+            { icone: "⚡", texto: "<strong>Antioxidante Natural:</strong> Rico em antocianinas, que protegem o sistema cardiovascular."},
+            { icone: "❤️", texto: "<strong>Fibras Solúveis:</strong> Auxiliam diretamente na saúde digestiva e no controle do colesterol."}
+
+        ]
+    },
+    "laranja": {
+        titulo: "LARANJA",
+        
+    }
+}
